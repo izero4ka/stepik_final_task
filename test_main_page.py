86232802e519120fe15cb47b974161b06926c0eb
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-14 11:34:01 +0300

    old link return

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,9 @@
 from .pages.main_page import MainPage
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
