8a5458b19c002e9b513a85568c5c44fc26a558ec
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-14 11:32:30 +0300

    added locators.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-
+        self.browser.implicitly_wait(timeout)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
     def open(self):
         self.browser.get(self.url)
---
+++ pages/locators.py
@@ -0,0 +1,4 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
