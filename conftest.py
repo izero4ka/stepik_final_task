85859bd4a1e7db88382483182d5baf5580259708
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-19 10:57:07 +0300

    final

--- conftest.py
+++ conftest.py
@@ -3,25 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
-                     help="Choose browser: chrome or firefox")
+                     help="Выберете браузер: Chrome или Firefox")
     parser.addoption('--language', action='store', default='en',
-                     help="Choose language")
+                     help="Выберете язык: ru, en или fr")
 @pytest.fixture(scope='function')
 def browser(request):
     browser_name = request.config.getoption('browser_name')
-    language = request.config.getoption('language')
+    user_language = request.config.getoption('language')
 
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+    if browser_name == "Chrome":
+        print("\nЗапуск браузера Chrome для теста..")
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+    elif browser_name == "Firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        print("\nЗапуск браузера Firefox для теста..")
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--browser_name должно быть Chrome или Firefox")
     yield browser
-    print("\nquit browser..")
+    print("\nЗакрытие браузера..")
     browser.quit()
