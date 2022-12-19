abb086b622e76af7701705f775a255c36d03b625
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-19 09:15:18 +0300

    Измения в переменных

--- test_main_page.py
+++ test_main_page.py
@@ -6,10 +6,11 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 def test_link_should_login(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
