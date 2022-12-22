5e486f8a940727b39ecbf47f04f00af85db0f996
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-22 09:58:42 +0300

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -5,3 +5,9 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
