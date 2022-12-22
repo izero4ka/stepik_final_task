9a68a1b440fe168cf6977e57d2d11ae93617081c
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-22 10:05:48 +0300

    add test_case

--- test_product_page.py
+++ test_product_page.py
@@ -1,1 +1,12 @@
-1
+from .pages.product_page import ProductPage
+class TestGuestAddToBasketFromProductPage():
+    def test_guest_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_message_about_adding()
+        page.should_be_message_basket_total()
+
+
