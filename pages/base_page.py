09e83a495e4db3e9eeb889f5b9368d9e817fa9cd
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-22 10:01:27 +0300

    add math

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
+import time
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -12,3 +15,18 @@ class BasePage():
         return True
     def open(self):
         self.browser.get(self.url)
+    # Формула для примера
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            time.sleep(120)
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
