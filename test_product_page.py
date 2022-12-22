d2c6d61f316b13b11680cce42325c60041c02405
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-22 11:42:27 +0300

    add new link

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 from .pages.product_page import ProductPage
 class TestGuestAddToBasketFromProductPage():
     def test_guest_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
         page.open()
         page.add_product_to_basket()
