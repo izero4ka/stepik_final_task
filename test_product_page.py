0fb8dcbbc8158f9a09b4fca1a4108477e697d7f7
Dmitriy Samarov <izero4ka@yandex.ru>
2022-12-26 10:50:49 +0300

    promo link

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,22 @@
 from .pages.product_page import ProductPage
+import pytest
 class TestGuestAddToBasketFromProductPage():
-    def test_guest_can_add_product_to_basket(self, browser):
-        #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    @pytest.mark.parametrize('link',
+                             ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                        pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                            marks=pytest.mark.xfail),
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+    def test_guest_can_add_product_to_basket(browser, link):
         page = ProductPage(browser, link)
         page.open()
         page.add_product_to_basket()
         page.solve_quiz_and_get_code()
         page.should_be_message_about_adding()
         page.should_be_message_basket_total()
-
-
